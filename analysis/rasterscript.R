1977f081ca6f8220d9f17b69aa554d872faaf58f
Uta Berger <bosquealves@users.noreply.github.com>
2019-02-25 10:05:50 +0100

    Update rasterscript.R
    I added the selection of burned and unburned cells (focal cell), but the hist shows all counted cells and not zhe average :-(

--- analysis/rasterscript.R
+++ analysis/rasterscript.R
@@ -4,7 +4,13 @@ crs(r15) <- CRS("+proj=robin +datum=WGS84")
 plot(r15)
 w <- matrix(c(0,1,0,1,0,1,0,1,0), nr=3,nc=3) #neighbourhood
 f <- focal(r15, w=w, pad=TRUE, padValue=0) #apply w
-plot(f)
+plot(f) #plot all cells
+
+#Sorry - we need the average for each cell but this gives us the number of all cells. I saw that you found
+# ab better solution. Can you insert this?
+hist(f, main = "all cells")         #hist of burned neighbour cells for all cells 
+hist(f[r15 == 1], main = "focal cells all burned", xlab = "# of burned cells") #hist of burned neighbour cells for burned cells only
+hist(f[r15 == 0], main = "focal cells all unburned", xlab = "# of burned cells") #hist of burned neighbour cells for not burned cells only
 
 
 #test
@@ -22,3 +28,9 @@ values(r)
 values(f)
 plot(f)
 hist(f)
+
+#separate the results of counting for cells which are originally burned (have a value of "1" ), and the ones which were orignally not burned (f-value == "0")
+f_burned <- f[r == 1]  #vector with number of burned neighbours for burned cells
+f_notburned <- f[r == 0]  #vector with number of burned neighbours for non-burned cells
+hist(f_burned)
+hist(f_notburned)
