63d560c524ccc5540564866ac41c560f9a8ef614
kirana <kirana.widyastuti@gmail.com>
2019-02-26 13:25:09 +0100

    update probability plotting

--- analysis/rasterscript.R
+++ analysis/rasterscript.R
@@ -12,6 +12,46 @@ hist(f, main = "all cells")         #hist of burned neighbour cells for all cell
 hist(f[r15 == 1], main = "focal cells all burned", xlab = "# of burned cells") #hist of burned neighbour cells for burned cells only
 hist(f[r15 == 0], main = "focal cells all unburned", xlab = "# of burned cells") #hist of burned neighbour cells for not burned cells only
 
+hist(focalr15)
+hist(focalr15[r15 == 1], main = "focal cells all burned", xlab = "# of burned cells")
+
+
+#Kirana: I try to put the calculation into a function
+nearest.neighbor4 <- function(map, focal.value) {
+  w <- matrix(c(0,1,0,1,0,1,0,1,0), nr=3,nc=3) #create 4 neighbourhood window
+  return(focal(map, w=w, pad=TRUE, padValue=0)[map==focal.value])
+}
+
+#use the function
+res.burned.focal <- nearest.neighbor4(r15,1)
+res.unburned.focal <- nearest.neighbor4(r15,0)
+
+#another function to summarize the probabilities of a focal matrix
+probsum <- function(focalres)
+{
+  c <- NULL
+  for(i in 0:max(focalres))
+  {
+    c <- rbind(c, cbind(i, length(focalres[focalres==i]) / length (focalres)))
+  }
+  df <- data.frame(c)
+  colnames(df) <- c("burned_patch", "probability")
+  return(df)
+}
+
+#use the function and plot it
+df.result <- probsum(res.burned.focal)
+
+library(ggplot2)
+# Basic barplot
+p<-ggplot(data=df.result, aes(x=burned_patch, y=probability)) +
+  geom_bar(stat="identity")
+p
+
+
+
+
+
 
 #test
 #create a raster file
@@ -34,3 +74,51 @@ f_burned <- f[r == 1]  #vector with number of burned neighbours for burned cells
 f_notburned <- f[r == 0]  #vector with number of burned neighbours for non-burned cells
 hist(f_burned)
 hist(f_notburned)
+
+#try the dummy raster with function
+f.burned.focal <- nearest.neighbor4(r,1)
+
+prob.f.burned.focal <- f.burned.focal / length(f.burned.focal)
+
+hist(prob.f.burned.focal)
+
+f.burned.focal[1]
+
+max(f.burned.focal)
+
+probs <- NULL
+
+
+c <- NULL
+probsum <- function(focalres)
+{
+  for(i in 0:max(focalres))
+  {
+    c <- rbind(c, cbind(i, length(focalres[focalres==i]) / length (focalres)))
+  }
+  return(c)
+}
+
+
+#function to summarize the probabilities of a focal matrix
+probsum <- function(focalres)
+{
+  c <- NULL
+  for(i in 0:max(focalres))
+  {
+    c <- rbind(c, cbind(i, length(focalres[focalres==i]) / length (focalres)))
+  }
+  df <- data.frame(c)
+  colnames(df) <- c("burned_patch", "probability")
+  return(df)
+}
+
+df.result <- data.frame(probsum(f.burned.focal))
+colnames(df.result) <- c("burned_patch", "probability")
+library(ggplot2)
+# Basic barplot
+p<-ggplot(data=df.result, aes(x=burned_patch, y=probability)) +
+  geom_bar(stat="identity")
+p
+
+
